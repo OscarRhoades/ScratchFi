1709b667ef513e1a2b2042326d0ef151247866c9
Oscar Rhoades <oscar.s.rhoades@gmail.com>
2023-03-04 15:48:27 -0700

    initial commit

---
+++ execute.py
@@ -0,0 +1,64 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def then(arg1, arg2):
+
+    plt.plot( arg1['Close_x'], 'o')
+    plt.plot( arg2['Close_x'], 'o')
+    plt.show()
+
+    
+    then = pd.merge(arg1, arg2, how = "left", on = "Date")
+    
+
+    print(len(arg1))
+    print(len(then.dropna()))
+    print(len(then))
+    print(then)
+
+    return then
+
+
+def ticker(tkr_str):
+    tkr = yf.Ticker(tkr_str).history(period="8mo")
+    
+    # print(tkr)
+    return tkr
+
+
+def n_time_average(tkr, days):
+    # numeric function
+    lastdayfrom = pd.Timestamp.now() 
+
+    # ndf = tkr.set_index('Date')
+    # #if datetimeindex isn't order, order it
+    # ndf = ndf.sort_index()
+    #last 30 days of date lastday
+
+    ndf = tkr.loc[lastdayfrom - pd.Timedelta(days=days):lastdayfrom].reset_index()
+
+    return ndf["Close"].mean()
+
+
+def compair_filter(arg1, arg2):
+    # filters arg1 when arg1 is greater than arg2
+    filter = pd.merge(arg1, arg2, on = "Date").dropna()
+    filter = filter.loc[filter["Close_x"] > filter["Close_y"]]
+    return filter[["Close_x"]]
+
+def moving_average(tkr, window):
+    # this gives a dataframe
+
+    # Make this open/close an advanced option parameter
+    moving_average = tkr["Close"].rolling(window=window).mean()
+    return moving_average
+
+
+# when amazon is higher than its moving average
+# print(len(ticker("BTC-USD")))
+# print(len(compair_filter(ticker("BTC-USD"), moving_average(ticker("BTC-USD"), 15))))
+
+# compute = then(compair_filter(ticker("AMZN"), moving_average(ticker("AMZN"), 30)), compair_filter(ticker("GLD"), moving_average(ticker("GLD"), 30)))
+
+
---
+++ parse.py
@@ -0,0 +1,52 @@
+
+import json 
+
+def then_json(arg1, arg2):
+    then = {
+        "type": "then",
+        "arg1": arg1,
+        "arg2": arg2
+    }
+    return then
+
+def ticker_json(tkr_str):
+    ticker = {
+        "type": "ticker",
+        "ticker_name": tkr_str,
+    }
+    return ticker
+
+def moving_average_json(arg1, window):
+    moving_average = {
+        "type": "moving_average",
+        "arg1": arg1,
+        "window": window
+        
+    }
+    return moving_average
+
+
+
+def compair_filter_json(arg1, arg2):
+    compair_filter = {
+        "type": "compair_filter",
+        "arg1": arg1,
+        "arg2": arg2
+    }
+    return compair_filter
+
+def oversold_json(arg1, window, p):
+    print("oversold")
+
+def derivative_json(arg1):
+    print("derivative")
+
+    
+
+object_program = then_json(compair_filter_json(ticker_json("AMZN"),moving_average_json(ticker_json("AMZN"), 30)), compair_filter_json(ticker_json("GLD"),moving_average_json(ticker_json("GLD"), 30)))
+
+json_object = json.dumps(object_program, indent = 4) 
+print(json_object)
+
+with open("sample.json", "w") as outfile:
+    json.dump(object_program, outfile)
